4896191d429561bbf7d4da9d42929f83538e82d9
mforte215 <mark.fortee@gmail.com>
2023-12-23 11:00:51 -0500

    first commit. base framework

---
+++ routes/index.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+
+
+
+//ROOT INDEX OF ALL APIS
+
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+
+const api = require('./routes/index');
+
+const PORT = 3001;
+
+const app = express();
+
+//set parsing type
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+//expose static files
+app.use(express.static('public'));
+
+app.use('/api', api);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+})
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+})
+
+app.listen(PORT, () => {
+    console.log('App listening at localhost:' + PORT);
+})
